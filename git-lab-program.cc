62dd014439545d63d793d5c9aef1267ac3236453
Makenna Travis <mt284622@ohio.edu>
2024-01-25 15:54:13 -0500

    code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Makenna Travis
+ *        Date: 25 January 2024
+ * Description: Git test code for CS2400 lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
